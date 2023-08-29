8fc278022d4b5e6a12d6ceccdee043e762b99102
lizozom <lizka.k@gmail.com>
2023-08-29 18:58:29 +0300

    Commn components

---
+++ components/common/input/index.tsx
@@ -0,0 +1,94 @@
+import { ErrorMessage } from '@hookform/error-message';
+import Eye from '@modules/common/icons/eye';
+import EyeOff from '@modules/common/icons/eye-off';
+import clsx from 'clsx';
+import React, { useEffect, useImperativeHandle, useState } from 'react';
+import { get } from 'react-hook-form';
+
+type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'placeholder'> & {
+  label: string;
+  errors?: Record<string, unknown>;
+  touched?: Record<string, unknown>;
+  name: string;
+};
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ type, name, label, errors, touched, required, ...props }, ref) => {
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const [showPassword, setShowPassword] = useState(false);
+    const [inputType, setInputType] = useState(type);
+
+    useEffect(() => {
+      if (type === 'password' && showPassword) {
+        setInputType('text');
+      }
+
+      if (type === 'password' && !showPassword) {
+        setInputType('password');
+      }
+    }, [type, showPassword]);
+
+    useImperativeHandle(ref, () => inputRef.current!);
+
+    const hasError = get(errors, name) && get(touched, name);
+
+    return (
+      <div>
+        <div className="text-base-regular relative z-0 w-full">
+          <input
+            type={inputType}
+            name={name}
+            placeholder=" "
+            className={clsx(
+              'mt-0 block w-full appearance-none border border-gray-200 bg-transparent px-4 pb-1 pt-4 focus:border-gray-400 focus:outline-none focus:ring-0',
+              {
+                'border-rose-500 focus:border-rose-500': hasError
+              }
+            )}
+            {...props}
+            ref={inputRef}
+          />
+          <label
+            htmlFor={name}
+            onClick={() => inputRef.current?.focus()}
+            className={clsx(
+              '-z-1 origin-0 absolute top-3 mx-3 px-1 text-gray-500 transition-all duration-300',
+              {
+                '!text-rose-500': hasError
+              }
+            )}
+          >
+            {label}
+            {required && <span className="text-rose-500">*</span>}
+          </label>
+          {type === 'password' && (
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-0 top-3 px-4 text-gray-400 outline-none transition-all duration-150 focus:text-gray-700 focus:outline-none"
+            >
+              {showPassword ? <Eye /> : <EyeOff />}
+            </button>
+          )}
+        </div>
+        {hasError && (
+          <ErrorMessage
+            errors={errors}
+            name={name}
+            render={({ message }) => {
+              return (
+                <div className="text-xsmall-regular pl-2 pt-1 text-rose-500">
+                  <span>{message}</span>
+                </div>
+              );
+            }}
+          />
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;
---
+++ components/common/underline-link/index.tsx
@@ -0,0 +1,25 @@
+import ArrowRight from '@modules/common/icons/arrow-right';
+import Link from 'next/link';
+
+type UnderlineLinkProps = {
+  href: string;
+  children?: React.ReactNode;
+};
+
+const UnderlineLink = ({ href, children }: UnderlineLinkProps) => {
+  return (
+    <div className="flex items-start">
+      <Link
+        href={href}
+        className="text-large-regular group flex items-center gap-x-4 border-b border-current py-2 transition-all duration-300 hover:pl-4 hover:pr-1"
+      >
+        <>
+          <span>{children}</span>
+          <ArrowRight size={20} className="transition-all duration-300 group-hover:ml-2" />
+        </>
+      </Link>
+    </div>
+  );
+};
+
+export default UnderlineLink;
